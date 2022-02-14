763aa99536e1c32c8b6ea4b9a51cb8e08cadc3ab
Mohammed Siraj Ansari <mohammedsiraj.ansari@intertrustgroup.com>
2022-02-14 20:50:21 +0530

    Updates

---
+++ precache-manifest.692e8eac6cb90a17dae137d8bc148e66.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f4afd473eeb35f93b1",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/css/main.1c13ec74.chunk.css"
+  },
+  {
+    "revision": "31f4afd473eeb35f93b1",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/js/main.31f4afd4.chunk.js"
+  },
+  {
+    "revision": "8a1efa17f85b39b2306b",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/js/1.8a1efa17.chunk.js"
+  },
+  {
+    "revision": "efea4d05de49e0400577",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/js/runtime~main.efea4d05.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "10d69662b4849a9246adc24cc632815b",
+    "url": "/Mohammed-Siraj-Ansari-NamshiInterview-20220214/index.html"
+  }
+];
